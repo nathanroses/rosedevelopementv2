811e3a7fc68656c8b850a6f54475edbd44d96b81
Nathan Rose <natetron99@gmail.com>
2023-11-07 13:56:58 -0500

    fix it

--- src/components/atoms/Logo.tsx
+++ src/components/atoms/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => (
   <div className="flex items-center">
     <Image src={logoImage} alt="Rose Development Logo" width={75} height={100} />
     <span className="self-center whitespace-nowrap text-2xl font-bold text-gray-900 dark:text-white md:text-xl">
-      Rose Developement 
+      Rose Development 
     </span>
   </div>
 );
