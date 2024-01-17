908938c297077e5c62060be82ea79e5daba68713
Nathan Rose <natetron99@gmail.com>
2024-01-16 21:34:05 -0500

    who'

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 import { findLatestPosts } from '~/utils/posts';
 
 export const metadata: Metadata = {
-  title: 'Projects',
+  title: 'Current Clients',
 };
 
 export default async function Home({}) {
@@ -15,7 +15,7 @@ export default async function Home({}) {
     <section className="mx-auto max-w-3xl px-6 py-12 sm:px-6 sm:py-16 lg:py-20">
       <header>
         <h1 className="leading-tighter font-heading mb-8 text-center text-4xl font-bold tracking-tighter md:mb-16 md:text-5xl">
-          Current Clients
+         Our Current Clients
         </h1>
       </header>
       <div className="grid grid-cols-1 gap-6  p-4 md:p-0 lg:grid-cols-2">
